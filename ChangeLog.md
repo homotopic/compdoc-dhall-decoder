23318637b4235027b75d214783baabe9dc115112
Daniel Firth <dan.firth@homotopic.tech>
2021-07-26 09:21:51 +0200

    Update compdoc to v0.3.0.0

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,5 +1,9 @@
 # Changelog for compdoc-dhall-decoder
 
+## v0.3.0.0
+
+* Update to compdoc-0.3.0.0
+
 ## v0.1.0.0
 
 * Working compdoc decoder for dhall.
--- src/Text/Compdoc/Dhall.hs
+++ src/Text/Compdoc/Dhall.hs
@@ -1,32 +1,33 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
+
 module Text.Compdoc.Dhall where
 
 import Composite.Aeson
 import Composite.Record
 import Data.Either.Validation
+import Data.Text as T
+import Data.Typeable
 import Data.Void
 import qualified Dhall as D
-import Data.Text as T
 import Text.Compdoc
-import Data.Typeable
 import Text.Pandoc
 
 -- | Decode a Compdoc value.
-compdocDecoder :: WriterOptions -> JsonFormat Void (Record a) -> D.InputNormalizer -> D.Decoder (Record (FContent ': a))
+compdocDecoder :: WriterOptions -> JsonFormat Void (Record a) -> D.InputNormalizer -> D.Decoder (Compdoc a)
 compdocDecoder wopts f opts =
-      D.Decoder
-            { D.extract = extractDoc
-            , D.expected = expectedDoc
-            }
-      where
-        docDecoder :: D.Decoder Text
-        docDecoder = D.autoWith opts
+  D.Decoder
+    { D.extract = extractDoc,
+      D.expected = expectedDoc
+    }
+  where
+    docDecoder :: D.Decoder Text
+    docDecoder = D.autoWith opts
 
-        extractDoc expression =
-          case D.extract docDecoder expression of
-              Success x -> case readMarkdown' def { readerExtensions = pandocExtensions } wopts f x of
-                Left exception   -> D.extractError (T.pack $ show exception)
-                Right path       -> Success path
-              Failure e        -> Failure e
-        expectedDoc = D.expected docDecoder
+    extractDoc expression =
+      case D.extract docDecoder expression of
+        Success x -> case readMarkdown' def {readerExtensions = pandocExtensions} wopts f x of
+          Left exception -> D.extractError (T.pack $ show exception)
+          Right l -> Success l
+        Failure e -> Failure e
+    expectedDoc = D.expected docDecoder
